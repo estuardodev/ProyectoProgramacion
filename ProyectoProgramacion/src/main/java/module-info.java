3c8615521266229b60c872555f8a36c180ae5e0e
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-02-04 12:49:51 -0600

    Se creó el proyecto

---
+++ ProyectoProgramacion/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.estuardodev.proyectoprogramacion {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.estuardodev.proyectoprogramacion to javafx.fxml;
+    exports com.estuardodev.proyectoprogramacion;
+}
