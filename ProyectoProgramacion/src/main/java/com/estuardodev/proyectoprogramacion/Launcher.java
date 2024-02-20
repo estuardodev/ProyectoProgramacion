d1bb7daa0be12fe4aeb17a994f7681c457bc7297
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-02-19 21:18:28 -0600

    Se creo el punto de partida para el .jar

---
+++ ProyectoProgramacion/src/main/java/com/estuardodev/proyectoprogramacion/Launcher.java
@@ -0,0 +1,9 @@
+package com.estuardodev.proyectoprogramacion;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args){
+        Application.launch(ProyectoApplication.class, args);
+    }
+}
